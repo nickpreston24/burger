4db0309e2d1d6a261d6590a78785bbd989732674
Michael Preston <michael.n.preston@gmail.com>
2019-01-12 18:47:44 -0600

    added API routes for burgers controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,38 @@
 var express = require('express');
+var router = express.Router();
 var burger = require('../models/burger.js');
-var app = express();
 
-// var router = ???;
-// module.exports = router;
+router.get("/", function (request, response) {
+    burger.all(function (data) {
+        var obj = {
+            burgers: data
+        };
+        console.log(obj);
+        response.render("index", obj);
+    });
+})
+
+router.post("/api/burgers", function (request, response) {
+    burger.create([
+        "name", "devoured"
+    ], [
+        request.body.name, request.body.devoured
+    ], function (result) {
+        response.json({
+            id: result.id
+        })
+    })
+})
+
+router.put("/api/burgers/:id", function (request, response) {
+    var condition = "id = " + request.params.id;
+    burger.update({}, condition, function (result) {
+        if (result.changedRows == 0) {
+            return response.status(404).end();
+        } else {
+            response.status(200).end();
+        }
+    })
+})
+
+module.exports = router;
