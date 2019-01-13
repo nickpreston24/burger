0ce927a3e7d5c3449d0f7ee8845da1ef849e3745
Michael Preston <michael.n.preston@gmail.com>
2019-01-12 21:52:27 -0600

    Added production load case

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 /*	Author: Michael Preston
  *	Created Date: "01-11-2019"
  */
-
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 8080;
 
-var PORT = process.env.PORT | 3000;
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
 
+app.use(express.static("public")); //for use with front-end materials under a /public/
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
-app.use(express.static("public")); //for use with front-end materials under a /public/
 
-// require("./routes/html-routes.js")(app);
-// require("./routes/burger-api-routes.js")(app);
+var exphbs = require('express-handlebars');
+
+/*Handlebars*/
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
